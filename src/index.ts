51f08e314a7387e172464318ee53cd9f82f3629e
konradkjems <konrad@tapfeed.dk>
2025-01-09 16:00:29 +0100

    Backend fixed 5

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 app.use(cors({
     origin: process.env.NODE_ENV === 'production' 
         ? process.env.FRONTEND_URL 
-        : ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:4173'],
+        : ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:4173', 'https://better-bets-frontend.vercel.app/'],
     credentials: true
 }));
 app.use(express.json());
